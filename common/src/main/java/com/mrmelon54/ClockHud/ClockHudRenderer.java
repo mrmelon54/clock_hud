f02fb27588f624dadee64475c05c49d52aa36b01
Bryan Yung <byung806@gmail.com>
2023-07-14 01:08:44 -0400

    Add 24 hour and 12 hour time display modes, add ability to show only clock icon

--- common/src/main/java/com/mrmelon54/ClockHud/ClockHudRenderer.java
+++ common/src/main/java/com/mrmelon54/ClockHud/ClockHudRenderer.java
@@ -1,6 +1,7 @@
 package com.mrmelon54.ClockHud;
 
 import com.mrmelon54.ClockHud.enums.ClockPosition;
+import com.mrmelon54.ClockHud.enums.GameTimeDisplayMode;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -14,36 +15,50 @@ public class ClockHudRenderer implements ClientGuiEvent.RenderHud {
     public void renderHud(GuiGraphics graphics, float tickDelta) {
         ConfigStructure config = ClockHud.getConfig();
         Minecraft client = Minecraft.getInstance();
-        if (!config.clockEnabled || client.options.renderDebug) return;
+        if (client.options.renderDebug) return;
+        
+        long offsettedTimeInTicks = (client.level.dayTime() + 6000) % 24000;
+        String minutes = String.format("%02d", (int) ((double)(offsettedTimeInTicks/10 % 100)/100*60));
 
-        String clockText = client.level != null ? String.valueOf(client.level.dayTime() % 24000L) : "";
+        String clockText = client.level != null ? switch (config.timeDisplayMode) {
+            case TICKS -> String.valueOf(client.level.dayTime() % 24000);
+            case HRS24 -> String.valueOf(offsettedTimeInTicks/1000) + ":" + minutes;
+            case HRS12 -> String.valueOf(((offsettedTimeInTicks/1000) + 11) % 12 + 1) + ":" + minutes + ((offsettedTimeInTicks/1000 >= 12) ? " PM" : " AM");
+            default -> "";
+        } : "";
         int textLength = client.font.width(clockText);
         int textHeight = client.font.lineHeight;
 
-        int clockSize = config.iconEnabled ? 16 : 0;
-        int clockGap = config.iconEnabled ? 2 : 0;
+        int clockSize = config.clockIconEnabled ? 16 : 0;
+        int clockGap = config.clockIconEnabled ? 5 : 0;
         int offsetForIcon = clockSize + clockGap;
         int iconOffset = config.iconPosition == ClockPosition.RIGHT ? textLength + clockGap : 0;
         int clockY = switch (config.position.getVerticalPosition()) {
             case NEGATIVE -> 0;
             case NEUTRAL -> (textHeight - clockSize) / 2;
             case POSITIVE -> textHeight - clockSize;
+            default -> 0;
         };
 
         int myX = switch (config.position.getHorizontalPosition()) {
             case NEGATIVE -> config.xOffset;
             case NEUTRAL -> graphics.guiWidth() / 2 - (textLength + offsetForIcon) / 2 + config.xOffset;
             case POSITIVE -> graphics.guiWidth() - (textLength + offsetForIcon) + config.xOffset;
+            default -> config.xOffset;
         };
         int myY = switch (config.position.getVerticalPosition()) {
             case NEGATIVE -> config.yOffset;
             case NEUTRAL -> graphics.guiHeight() / 2 - textHeight / 2 + config.yOffset;
             case POSITIVE -> graphics.guiHeight() - textHeight + config.yOffset;
+            default -> config.yOffset;
         };
 
         if (clockItemStack == null) clockItemStack = new ItemStack(Items.CLOCK);
-        if (config.iconEnabled)
+        if (config.gameTimeDisplayEnabled) {
+            graphics.drawString(client.font, clockText, myX + (config.iconPosition == ClockPosition.LEFT ? offsetForIcon : 0), myY, config.color);
+        }
+        if (config.clockIconEnabled) {
             graphics.renderItem(clockItemStack, myX + iconOffset, myY + clockY, 0);
-        graphics.drawString(client.font, clockText, myX + (config.iconPosition == ClockPosition.LEFT ? offsetForIcon : 0), myY, config.colour);
+        }
     }
 }
