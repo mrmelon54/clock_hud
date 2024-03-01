ef758aa8e739dd3994f185a54f35b34a35d74e84
MrMelon54 <github@mrmelon54.com>
2024-03-01 22:07:27 +0000

    Move publishbuilds script into workflow

--- publishbuilds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while read -r filename; do
-  curl --fail -X POST -H "Authorization: Bearer ${{ secrets.DEPLOY }}" -F "upload=@$filename" https://api.mrmelon54.com/v1/mc-upload/upload/clock-hud
-done < <(find ./buildAllJars/ -name '*.jar' -type f)
